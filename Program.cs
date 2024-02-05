cbf303a246381434e8642d36d3c6e7e9d7233e9f
Logan Reeves <loganreeves1@yahoo.com>
2024-02-04 19:32:20 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.Design;
+
+namespace Circle_Objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius;
+            bool loop1 = true;
+            Console.WriteLine("Welcome to the Circle tool. Please enter in the radius of your circle.");
+            while (double.TryParse(Console.ReadLine(), out radius) == false || radius <= 0)
+            {
+                Console.WriteLine("Error: Please enter a valid radius");
+            }
+
+           Circle circle1 = new Circle(radius);
+            while (loop1)
+            {
+                Console.WriteLine($"The diameter is: {circle1.CalculateDiameter(circle1.GetRadius())}");
+                Console.WriteLine($"The circumference is: {circle1.CalculateCircumference(circle1.GetRadius())}");
+                Console.WriteLine($"The Area is: {circle1.CalculateArea(circle1.GetRadius())}");
+                Console.WriteLine("Should the circle grow? ('y' or 'n')");
+                string choice = Console.ReadLine().ToLower().Trim();
+                if (choice == "y")
+                {
+                    Console.WriteLine("The circle is magically growing.");
+                    circle1.Grow(circle1.GetRadius());
+                }
+                else if (choice == "n")
+                {
+                    Console.WriteLine($"Goodbye. The ciricle's final radius is {circle1.GetRadius()}");
+                    loop1 = false;
+
+                }
+                else
+                {
+                    Console.WriteLine("Please only enter'y' or 'n'");
+                }
+            }
+        }
+    } 
+}
---
+++ circle.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_Objects
+{
+    internal class Circle
+    {
+        //properties
+        private double Radius { get; set; }
+
+        //Constructer 
+        public  Circle (double _radius)
+        {
+            Radius = _radius;
+        }
+
+        //METHODS
+
+        //Diameter
+        public double CalculateDiameter(double radius)
+        {
+            double diameter = 2 * radius;
+            return diameter;
+        }
+
+        //Circumference
+        public double CalculateCircumference(double radius)
+        {
+            double circumference = 2 * Math.PI * radius;
+            return circumference;
+        }
+
+        //Area 
+        public double CalculateArea (double radius)
+        {
+            double area = Math.PI * radius * radius;
+            return area;
+        }
+
+        //Grow
+        public void Grow(double radius)
+        {
+           Radius = 2 * radius;
+        }
+        // final radius 
+        public double GetRadius()
+        {
+            return Radius;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
